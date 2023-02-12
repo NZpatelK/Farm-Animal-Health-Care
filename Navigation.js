6a1e496442b893ea4886ac13c7594522aad1d94a
NZpatelK <patelk.dev@hotmail.com>
2023-02-12 19:08:49 +1300

    add tab navigation function and connect two screens

--- Navigation.js
+++ Navigation.js
@@ -1,36 +1,18 @@
 import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { StyleSheet, View, Image, Text, TouchableOpacity } from 'react-native'
+import { StyleSheet} from 'react-native';
+import DashboardScreen from "./screens/DashboardScreen";
+import AnimalHealthScreen from "./screens/AnimalHealthScreen";
 
 const Tab = createBottomTabNavigator();
 
-const CustomTabBarButton = ({ children, onPress }) => (
-    <TouchableOpacity
-    style={{
-        top: 0,
-        justifyContent: "center",
-        alignContent:"center",
-        ...style.shadow
-    }}
-    onPress={onPress}
-    >
-        <View style={{
-            width: 80,
-            height: 80,
-            borderRadius: 40,
-            backgroundColor: '#e32f45'
-        }}>
-            {children}
-        </View>
-    </TouchableOpacity>
-);
-
 export default function Navigation() {
-  return (
-    <View>
-      <Text>Navigation</Text>
-    </View>
-  )
+    return (
+        <Tab.Navigator>
+            <Tab.Screen name="dashboard" component={DashboardScreen} />
+            <Tab.Screen name="Animal Health" component={AnimalHealthScreen} />
+        </Tab.Navigator>
+    )
 }
 
 const styles = StyleSheet.create({})
---
+++ screens/AnimalHealthScreen.js
@@ -0,0 +1,12 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+export default function AnimalHealthScreen() {
+  return (
+    <View>
+      <Text>AnimalHealthScreen</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
