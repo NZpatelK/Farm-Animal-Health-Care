12347e2c9ba514f5dec7050efa3ffba6a8201cb2
NZpatelK <patelk.dev@hotmail.com>
2023-02-12 19:44:43 +1300

    create the animal to display list of animal

--- screens/AnimalHealthScreen.js
+++ screens/AnimalHealthScreen.js
@@ -1,12 +1,36 @@
 import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useNavigation } from '@react-navigation/native'
+import axios from 'axios';
+import { ScrollView } from 'react-native';
 
 export default function AnimalHealthScreen() {
-  return (
-    <View>
-      <Text>AnimalHealthScreen</Text>
-    </View>
-  )
+
+    const nav = useNavigation();
+    const [animalData, setAnimalData] = useState([{}]);
+
+    useEffect(() => {
+        axios.get("/Users/karanpatel/Desktop/Project/FarmAnimalHealthCare/FarmAnimalHealthCare/data/AnimalData.json")
+            .then(response => {
+                setAnimalData(response.data);
+            })
+            .catch(error => {
+                console.error(error);
+            });
+    }, []);
+
+
+    return (
+        <ScrollView>
+            <View>
+                {Array.isArray(animalData) && animalData.map(animal => (
+                    <View>
+                        <Text>{animal.health}</Text>
+                    </View>
+                ))}
+            </View>
+        </ScrollView>
+    )
 }
 
 const styles = StyleSheet.create({})
