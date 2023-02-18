9a41439b2eaa46264df76efa5f86efe7a762442b
NZpatelK <patelk.dev@hotmail.com>
2023-02-19 11:32:55 +1300

    install the reaminated and add function in ListItem

--- components/ListItem.js
+++ components/ListItem.js
@@ -1,12 +1,43 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet} from 'react-native'
+import Animated, { useAnimatedStyle, withTiming } from 'react-native-reanimated'
 import React from 'react'
 
-export default function ListItem({item, viewableItems, children}) {
-  return (
-    <View>
-      <Text>ListItem</Text>
-    </View>
-  )
+export default function ListItem({ item, viewableItems, children }) {
+
+    const rStyle = useAnimatedStyle(() => {
+        const isVisible = Boolean(
+            viewableItems.value
+                .fiter((item) => item.isViewable)
+                .find((viewableItems) => viewableItems.item.id === item.id)
+        );
+
+        return {
+            opacity: withTiming(isVisible ? 1 : 0),
+            transform: [
+                {
+                    scale: withTiming(isVisible ? 1 : 0.6),
+                },
+            ],
+        };
+    }, []);
+
+    return (
+        <Animated.View
+            style={[
+                {
+                    height: 100,
+                    width: '90%',
+                    backgroundColor: '#78CAD2',
+                    alignSelf: 'center',
+                    borderRadius: 15,
+                    marginTop: 20,
+                },
+                rStyle,
+            ]}
+        >
+            {children}
+        </Animated.View>
+    )
 }
 
 const styles = StyleSheet.create({})
