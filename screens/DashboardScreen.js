cac759156541cfc8f1d97466461b62b168218d49
NZpatelK <patelk.dev@hotmail.com>
2023-04-01 20:46:58 +1300

    Update DashboardScreen.js

--- screens/DashboardScreen.js
+++ screens/DashboardScreen.js
@@ -1,6 +1,11 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
+
+//This Dashboard Screen
+//This screen will dsiplay all different chart and data of farm and farm animal. 
+//This screen I am not foucs on it and I am more foucs on montioring the farm animal. 
+//In the future will come to this.
 export default function DashboardScreen() {
   return (
     <View style={styles.container}>
