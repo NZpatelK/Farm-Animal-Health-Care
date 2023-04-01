c170800c3f1bb0109844005c64cec056419d7493
NZpatelK <patelk.dev@hotmail.com>
2023-04-01 18:04:02 +1300

    Comment on donut chart file

--- components/DonutChart.js
+++ components/DonutChart.js
@@ -1,21 +1,29 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, View } from 'react-native'
 import React, { useState, useEffect } from 'react';
-import { VictoryPie, VictoryLabel } from 'victory-native';
-import { Svg } from 'react-native-svg';
+import { VictoryPie } from 'victory-native';
 
-const graphicColor = ['red', '#e6e300', '#00cc10']; // Colors
-const defaultGraphicData = [{ y: 0 }, { y: 0 }, { y: 100 }];
+/*
+This class is to set up display of donut chart.
+It is only allow 3 values to display the chart.
+It not have labels and title on the chart. 
+*/
+
+const graphicColor = ['red', '#e6e300', '#00cc10']; // Colors of donut chart 
+const defaultGraphicData = [{ y: 0 }, { y: 0 }, { y: 100 }]; // not acutal data to set the defualt setting. It is part for amination process.
 
 export default function DonutChart({getGraphicData}) {
+
     const [graphicData, setGraphicData] = useState(defaultGraphicData);
 
     useEffect(() => {
-        setGraphicData(getGraphicData); // Setting the data that we want to display
+        setGraphicData(getGraphicData); // fetch and setting the actual data that we want to display in donut chart.
     }, []);
 
     return (
 
         <View style={{ justifyContent: "center"}}>
+
+            {/* Donut chart display */}
             <VictoryPie
                 padding={10}
                 animate={{ easing: 'exp' }}
